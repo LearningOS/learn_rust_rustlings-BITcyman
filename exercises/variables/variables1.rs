d824b7cea9b3ac2875b4b2a85961aa048768c39c
BITcyman <815207911@qq.com>
2022-10-28 07:06:35 +0000

    update

--- exercises/variables/variables1.rs
+++ exercises/variables/variables1.rs
@@ -1,10 +1,9 @@
 // variables1.rs
 // Make me compile!
-// Execute the command `rustlings hint variables1` if you want a hint :)
+// Execute `rustlings hint variables1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn main() {
-    x = 5;
+    let x = 5;
     println!("x has the value {}", x);
 }
